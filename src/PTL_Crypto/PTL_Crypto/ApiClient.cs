99c4e8229ecf97b526c03bbec08e18fffd115a89
YOSEF NADEMO <72689921+Josefnademo@users.noreply.github.com>
2025-09-05 16:05:23 +0200

    Maquette Figma, JDT
    plus de détails dans JDT

---
+++ src/PTL_Crypto/PTL_Crypto/ApiClient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTL_Crypto
+{
+    internal class ApiClient
+    {
+    }
+}
