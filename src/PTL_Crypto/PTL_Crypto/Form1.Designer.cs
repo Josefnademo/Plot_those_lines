876cf1f396373cdc238420e8a23dcdc9ab4c2070
YOSEF NADEMO <72689921+Josefnademo@users.noreply.github.com>
2025-09-05 16:33:17 +0200

    Codage creation du projet vs, premier graphique
    Début du codage du projet : création et modélisation des Windows Forms, création des classes, et première mise en place d’une interface graphique évolutive.

--- src/PTL_Crypto/PTL_Crypto/Form1.Designer.cs
+++ src/PTL_Crypto/PTL_Crypto/Form1.Designer.cs
@@ -29,7 +29,58 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            formsPlot1 = new ScottPlot.WinForms.FormsPlot();
             SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(630, 504);
+            button1.Name = "button1";
+            button1.Size = new Size(230, 64);
+            button1.TabIndex = 0;
+            button1.Text = "Importer un fichier";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(115, 70);
+            button2.Name = "button2";
+            button2.Size = new Size(40, 25);
+            button2.TabIndex = 1;
+            button2.Text = "7j";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(161, 70);
+            button3.Name = "button3";
+            button3.Size = new Size(40, 25);
+            button3.TabIndex = 2;
+            button3.Text = "30j";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(207, 70);
+            button4.Name = "button4";
+            button4.Size = new Size(40, 25);
+            button4.TabIndex = 3;
+            button4.Text = "1a";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // formsPlot1
+            // 
+            formsPlot1.DisplayScale = 1F;
+            formsPlot1.Location = new Point(115, 101);
+            formsPlot1.Name = "formsPlot1";
+            formsPlot1.Size = new Size(745, 388);
+            formsPlot1.TabIndex = 4;
+            formsPlot1.Load += formsPlot1_Load;
             // 
             // Form1
             // 
@@ -39,12 +90,24 @@
             BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
             BackgroundImageLayout = ImageLayout.Zoom;
             ClientSize = new Size(975, 649);
+            Controls.Add(formsPlot1);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
             Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "Form1";
             Text = "PTL_Crypto";
+            Load += Form1_Load;
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private ScottPlot.WinForms.FormsPlot formsPlot1;
     }
 }
--- src/PTL_Crypto/PTL_Crypto/Form1.cs
+++ src/PTL_Crypto/PTL_Crypto/Form1.cs
@@ -1,3 +1,4 @@
+using ScottPlot;
 namespace PTL_Crypto
 {
     public partial class Form1 : Form
@@ -5,6 +6,26 @@ namespace PTL_Crypto
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void formsPlot1_Load(object sender, EventArgs e)
+        {
+            double[] dataX = new double[] { 1, 2, 3, 4, 5 };
+            double[] dataY = new double[] { 1, 12, 23, 24, 35 };
+
+            formsPlot1.Plot.AddScatterLines(dataX, dataY);// ligne avec points
+            // formsPlot1.Plot.AddScatterPoints(dataX, dataY); // seulement points
+            formsPlot1.Refresh();
         }
     }
 }
