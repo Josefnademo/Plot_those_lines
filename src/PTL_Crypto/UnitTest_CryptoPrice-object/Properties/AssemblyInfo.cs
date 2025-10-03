f7164ec7f03b636bf6de8617fb3e2e67833d6861
YOSEF NADEMO <72689921+Josefnademo@users.noreply.github.com>
2025-10-03 16:34:34 +0200

    unit test (CryptoPrice object)

---
+++ src/PTL_Crypto/UnitTest_CryptoPrice-object/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest_CryptoPrice-object")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("DGEP")]
+[assembly: AssemblyProduct("UnitTest_CryptoPrice-object")]
+[assembly: AssemblyCopyright("Copyright © DGEP 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1975971d-d606-422a-8b4a-4d51961211a0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/PTL_Crypto/UnitTest_CryptoPrice/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace UnitTest_CryptoPrice
+{
+    public class CryptoPrice
+    {
+        public string Symbol { get; set; }
+        public DateTime Timestamp { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class CryptoPriceTests
+    {
+        [Fact]
+        public void CryptoPrices_AreValidAndTemporalOrderCorrect()
+        {
+            // Arrange: create a list of test data
+            var prices = new List<CryptoPrice>
+            {
+                new CryptoPrice { Symbol = "BTC", Timestamp = DateTime.UtcNow.AddMinutes(-5), Price = 49000m },
+                new CryptoPrice { Symbol = "BTC", Timestamp = DateTime.UtcNow.AddMinutes(-3), Price = 49500m },
+                new CryptoPrice { Symbol = "BTC", Timestamp = DateTime.UtcNow.AddMinutes(-1), Price = 50000m }
+            };
+
+            // Act & Assert
+            for (int i = 0; i < prices.Count; i++)
+            {
+                Assert.False(string.IsNullOrWhiteSpace(prices[i].Symbol), $"Symbol at index {i} is empty");
+                Assert.True(prices[i].Price > 0, $"Price at index {i} is not positive");
+                Assert.True(prices[i].Timestamp <= DateTime.UtcNow, $"Timestamp at index {i} is in the future");
+
+                if (i > 0)
+                {
+                    Assert.True(prices[i].Timestamp > prices[i - 1].Timestamp,
+                        $"Timestamp at index {i} is not later than previous one");
+                }
+            }
+        }
+    }
+}
